2e942bf408ed2b9253a4dad19f77287923e849c2
rajatdrinfosoft-web <rajat.drinfosoft@gmail.com>
2025-11-29 16:24:47 +0530

    some changes

--- pilgrim-packge/config.py
+++ pilgrim-packge/config.py
@@ -1,5 +1,7 @@
+import os
+
 class Config:
-    SECRET_KEY = 'dev-secret-key-change-in-production'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://neondb_owner:npg_uYcyz7nM5Wqh@ep-shy-snow-adyq945n-pooler.c-2.us-east-1.aws.neon.tech/pilgrimpackages?sslmode=require&channel_binding=require'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-production')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://neondb_owner:npg_uYcyz7nM5Wqh@ep-shy-snow-adyq945n-pooler.c-2.us-east-1.aws.neon.tech/pilgrimpackages?sslmode=require&channel_binding=require')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CACHE_TYPE = 'simple'
